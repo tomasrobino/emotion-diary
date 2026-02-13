58b6b96f3611dcbb80cdf922470d7ec441fcd244
Tomas Robino <tomaserobino@gmail.com>
2026-02-13 08:51:07 +0100

    initial commit

---
+++ src/main/java/com/example/emotion_diary/EmotionDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.emotion_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/emotion_diary/EmotionDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emotion_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
